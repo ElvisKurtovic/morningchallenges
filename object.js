2c75751f7de582c2948b3503263307c088980ac8
ElvisKurtovic <elvis.kurtovic23@gmail.com>
2021-02-10 14:48:58 -0700

    morning challenge

--- object.js
+++ object.js
@@ -29,4 +29,61 @@ function greet(greeting){
     console.log(greeting)
 }
 
-greet("hello")
+let fruit = [
+    {
+        name: "Apple",
+        cost: 1
+    },
+    {
+        name: "Orange",
+        cost: 2
+    },
+    {
+        name: "Banana",
+        cost: 3,
+        ripe: true
+    }
+]
+
+fruit[0].name
+fruit[0]["name"]
+console.log(fruit[0]);
+
+greet("baby come back.. you can blame it all on me")
+
+// let userInput = window.prompt("Pick and action and an option; Actions; (G)o, (L)ook;")
+// userInput = userInput.split(" ")
+// let userAction = userInput[0]
+// let userOption = userInput[1]
+// let playing = true
+
+// while (playing) {
+    
+// }
+
+// switch (userAction) {
+//     case "g":
+//     case "G":
+//     case "GO":
+//     case "Go":
+//     case "gO":
+//     case "go":
+//         move(userOption)
+        
+//         break;
+//     case "look":
+//         console.log("You look around or something")
+//         break;
+//     case "q":
+//         playing = false
+//         console.log("Thanks for playing our super cool complicated game")
+//         break;
+
+//     default:
+//         console.log("I dont know")
+// }
+
+// function move(direction) {
+//     console.log("You move to the ", direction)
+// }
+
