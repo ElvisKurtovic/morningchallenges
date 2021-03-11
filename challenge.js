d431f9935e5aede67723c3c0d5074f71a16c886b
ElvisKurtovic <elvis.kurtovic23@gmail.com>
2021-03-11 09:47:07 -0700

    new challenge

--- challenge.js
+++ challenge.js
@@ -61,52 +61,63 @@
 // }
 
 
-let testData = [
-    "eedadn",
-    "drvtee",
-    "eandsr",
-    "raavrd",
-    "atevrs",
-    "tsrnev",
-    "sdttsa",
-    "rasrtv",
-    "nssdts",
-    "ntnada",
-    "svetve",
-    "tesnvt",
-    "vntsnd",
-    "vrdear",
-    "dvrsen",
-    "enarar",
-]
-function repeaterCode(arr) {
-    let out = ''
-    for (let col = 0; col < arr[0].length; col++) {
-        let dict = {}
-        let largest = ''
-        let largestCount = 0
-        arr.forEach(str => {
-            let char = str[col]
-            if (!dict[char]) {
+// let testData = [
+//     "eedadn",
+//     "drvtee",
+//     "eandsr",
+//     "raavrd",
+//     "atevrs",
+//     "tsrnev",
+//     "sdttsa",
+//     "rasrtv",
+//     "nssdts",
+//     "ntnada",
+//     "svetve",
+//     "tesnvt",
+//     "vntsnd",
+//     "vrdear",
+//     "dvrsen",
+//     "enarar",
+// ]
+// function repeaterCode(arr) {
+//     let out = ''
+//     for (let col = 0; col < arr[0].length; col++) {
+//         let dict = {}
+//         let largest = ''
+//         let largestCount = 0
+//         arr.forEach(str => {
+//             let char = str[col]
+//             if (!dict[char]) {
 
-                dict[char] = 1
-            } else {
-                dict[char]++
-            }
-            if (dict[char] > largestCount) {
-                largest = char
-                largestCount = dict[char]
-            }
-        }
+//                 dict[char] = 1
+//             } else {
+//                 dict[char]++
+//             }
+//             if (dict[char] > largestCount) {
+//                 largest = char
+//                 largestCount = dict[char]
+//             }
+//         }
 
-        )
-        out += largest
-    }
-    return out
-}
-repeaterCode(testData)
+//         )
+//         out += largest
+//     }
+//     return out
+// }
+// repeaterCode(testData)
 // function maxChar(str) {
 //     const obj = {};
 //     str.split('').forEach(char => obj[char] + 1 || 1);
 //     return Object.keys(obj).reduce((prev, next) => obj[a] >= obj[b] ? a : b);
 // }
+
+
+function sumMult() {
+    let num = 0
+    for (let i = 1; i <= 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            num += i
+        }
+    }
+    return num
+} 
